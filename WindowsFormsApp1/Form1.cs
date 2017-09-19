0c61fc5f2373d62be43940a44880a73727257cc4
Shaun Reynolds <405001@edu.cwa.ac.uk>
2017-09-19 10:46:11 +0100

    Added form to my button

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,5 +21,11 @@ namespace WindowsFormsApp1
         {
             // do sdtuff
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Show(this);
+        }
     }
 }
