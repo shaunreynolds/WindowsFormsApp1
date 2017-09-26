ea2108c58abe7e195b114359cfe6fce949a28e94
Roland <401627@edu.cwa.ac.uk>
2017-09-26 09:43:41 +0100

    unitestADDED

---
+++ RolandUnitest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RolandUnitest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RolandUnitest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("430330fd-b17d-4ff9-bc72-e2b5b829b421")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RolandUnitest/RolandTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp1;
+namespace RolandUnitest
+{
+    [TestClass]
+    public class RolandTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            myClass test = new myClass();
+
+            test.add(5);
+            test.add(0);
+
+
+
+        }
+    }
+}
